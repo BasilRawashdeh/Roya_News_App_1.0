192f08fd02d1f5bd08d43af7ffbe98a08ced09ef
Basil Rawashdeh <basilrawa31@gmail.com>
2021-08-25 18:06:32 +0300

    -layouts: activity_main.xml news_item.xml
    -gradle:
    picasso added
    
    MainActivity
    NewsAdapter
    NewsDetailsActivity
    
    are done.

--- app/src/main/java/com/example/royanewsapp/MainActivity.java
+++ app/src/main/java/com/example/royanewsapp/MainActivity.java
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelStoreOwner;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.example.royanewsapp.Model.NewsModel;
@@ -20,17 +25,136 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        SwipeRefreshLayout swipeRefreshLayout = findViewById(R.id.swipeRefresh);
+        RecyclerView newsRecyclerView = findViewById(R.id.newsRecyclerView);
+        newsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        newsRecyclerView.setHasFixedSize(true);
 
-
+        final NewsAdapter newsAdapter = new NewsAdapter();
+        newsRecyclerView.setAdapter(newsAdapter);
 
         newsViewModel = new ViewModelProvider(this).get(NewsViewModel.class);
         newsViewModel.getAllNewsLiveData().observe(this, new Observer<List<NewsModel>>() {
             @Override
             public void onChanged(List<NewsModel> newsModels) {
-                ///todo: update recyclerView
+                ///todo: update newsRecyclerView
                 Toast.makeText(getApplicationContext(), "MainActivity-onChanged", Toast.LENGTH_SHORT).show();
+                newsAdapter.setNews(newsModels);
+            }
+        });
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                ///todo: retrieveJson
             }
         });
 
     }
+/*
+
+    private class JsonTask extends AsyncTask<String, String, String> {
+
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            pd = new ProgressDialog(MainActivity.this);
+            pd.setMessage("Please wait");
+            pd.setCancelable(false);
+            pd.show();
+        }
+
+        protected String doInBackground(String... params) {
+
+
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+
+                InputStream stream = connection.getInputStream();
+
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+                String line = "";
+
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line+"\n");
+                    Log.d("Response: ", "> " + line);   //here u ll get whole response...... :-)
+
+                }
+
+                return buffer.toString();
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            if (pd.isShowing()){
+                pd.dismiss();
+            }
+            txtJson.setText(result);
+        }
+    }
+}
+
+    public void retrieveJson(String query ,String country, String apiKey){
+
+        swipeRefreshLayout.setRefreshing(true);
+        Call<Headlines> call;
+        if (!searchEditText.getText().toString().equals("")){
+            call= ApiClient.getInstance().getApi().getSpecificData(query,apiKey);
+        }else{
+            call= ApiClient.getInstance().getApi().getSpecificData("news",apiKey);
+            //call= ApiClient.getInstance().getApi().getHeadlines("Jo",apiKey);
+
+        }
+
+        call.enqueue(new Callback<Headlines>() {
+            @Override
+            public void onResponse(Call<Headlines> call, Response<Headlines> response) {
+                Log.i("MainActivity", "callBack Responded");
+                if (response.isSuccessful() && response.body().getArticles() != null){
+                    swipeRefreshLayout.setRefreshing(false);
+                    articles.clear();
+                    articles = response.body().getArticles();
+                    adapter = new Adapter(MainActivity.this,articles);
+                    recyclerView.setAdapter(adapter);
+                    adapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Headlines> call, Throwable t) {
+                Log.i("MainActivity", "callBack failed to Respond");
+                swipeRefreshLayout.setRefreshing(false);
+                Toast.makeText(MainActivity.this, t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    */
 }
---
+++ app/src/main/java/com/example/royanewsapp/NewsAdapter.java
@@ -0,0 +1,115 @@
+package com.example.royanewsapp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.royanewsapp.Model.NewsModel;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.NewsViewHolder> {
+
+    Context context;
+    List<NewsModel> newsModels = new ArrayList<>();
+
+
+    public NewsAdapter() {
+    }
+
+    public NewsAdapter(Context context, List<NewsModel> newsModels) {
+        this.context = context;
+        this.newsModels = newsModels;
+    }
+
+    @NonNull
+    @Override
+    public NewsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View newsView = LayoutInflater.from(parent.getContext()).inflate(R.layout.news_item, parent, false);
+        return new NewsViewHolder(newsView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final NewsViewHolder newsViewHolder, int position) {
+
+        final NewsModel newsModel = newsModels.get(position);
+
+        newsViewHolder.newsTitle.setText(newsModel.getNewsTitle());
+        newsViewHolder.newsSectionName.setText(newsModel.getNewsSectionName());
+        Picasso.with(context).load(newsModel.getNewsImage()).into(newsViewHolder.newsImageView);
+
+
+        newsViewHolder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, NewsDetailsActivity.class);
+                intent.putExtra("newsTitle",newsModel.getNewsTitle());
+                intent.putExtra("newsImageUrl",newsModel.getNewsImage());
+                //intent.putExtra("time",dateTime(newsModel.getPublishedAt()));
+                //intent.putExtra("desc",newsModel.getDescription());
+                //intent.putExtra("url",newsModel.getUrl());
+                context.startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return newsModels.size();
+    }
+
+    public void setNews(List<NewsModel> newsModels) {
+        this.newsModels = newsModels;
+        notifyDataSetChanged();
+    }
+
+    public class NewsViewHolder extends RecyclerView.ViewHolder {
+        TextView newsTitle, newsSectionName, newsDate;
+        ImageView newsImageView;
+        CardView cardView;
+
+        public NewsViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            newsTitle = itemView.findViewById(R.id.tvTitle);
+            newsSectionName = itemView.findViewById(R.id.tvSource);
+            newsDate = itemView.findViewById(R.id.tvDate);
+            newsImageView = itemView.findViewById(R.id.image);
+            cardView = itemView.findViewById(R.id.cardView);
+
+        }
+    }
+
+/*
+    public String dateTime(String t){
+        PrettyTime prettyTime = new PrettyTime(new Locale(getCountry()));
+        String time = null;
+        try {
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:",Locale.ENGLISH);
+            Date date = simpleDateFormat.parse(t);
+            time = prettyTime.format(date);
+        }catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return time;
+
+    }
+*/
+    public String getCountry(){
+        Locale locale = Locale.getDefault();
+        String country = locale.getCountry();
+        return country.toLowerCase();
+    }
+}
